28950e04df3a1c22c88d91a4c78ced457ee99292
Jonathan Eberle <jeberle@2-D-1.42heilbronn.de>
2024-04-25 04:37:14 +0200

    tried algos for optimization

--- put.c
+++ put.c
@@ -6,7 +6,7 @@
 /*   By: jeberle <jeberle@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 21:54:05 by jonathanebe       #+#    #+#             */
-/*   Updated: 2024/04/24 19:47:29 by jeberle          ###   ########.fr       */
+/*   Updated: 2024/04/25 02:13:32 by jeberle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,17 +50,6 @@ void put_content(void *n)
 	ft_putnbr(toput->abs_cost);
 	write (1, " dist: ", 7);
 	ft_putnbr(toput->distance);
-	//write (1, "num_data: ", 10);
-	//ft_putnbr(toput->num_data);
-	//write (1, "\nmove_cost_a: ", 14);
-	//ft_putnbr(toput->move_cost_a);
-	//write (1, "\nmove_cost_b: ", 14);
-	//ft_putnbr(toput->move_cost_b);
-	//write (1, "\nposition: ", 11);
-	//ft_putnbr(toput->position);
-	//write (1, "\nabs_cost: ", 19);
-	//ft_putnbr(toput->abs_cost);
-	//write (1, "\n", 1);
 }
 
 void put_short(void *n)
@@ -71,15 +60,15 @@ void put_short(void *n)
 		return ;
 	toput = (t_psu *)n;
 	ft_putnbr(toput->num_data);
-	//write (1, "num_data: ", 10);
-	//ft_putnbr(toput->num_data);
-	//write (1, "\nmove_cost_a: ", 14);
-	//ft_putnbr(toput->move_cost_a);
-	//write (1, "\nmove_cost_b: ", 14);
-	//ft_putnbr(toput->move_cost_b);
-	//write (1, "\nposition: ", 11);
-	//ft_putnbr(toput->position);
-	//write (1, "\nabs_cost: ", 19);
-	//ft_putnbr(toput->abs_cost);
-	//write (1, "\n", 1);
 }
+
+//ft_dlstput(a, put_content, '\n');
+//write(1, "\n#\n", 3);
+//ft_dlstput(b, put_content, '\n');
+//write(1, "\n___________________\n", 21);
+
+//write(1, "\n_______END_________\n", 21);
+//ft_dlstput(stack_a, put_short, ' ');
+//write(1, "\n#\n", 3);
+//ft_dlstput(stack_b, put_short, ' ');
+//write(1, "\n___________________\n", 21);
